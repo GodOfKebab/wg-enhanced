365616020448dfbb33c34bc620d6b67f65a208f3
Yaşar İdikut <yasar8000@gmail.com>
2022-09-25 08:51:07 -0600

    UI work on removing connections (in the edit and create window)

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -525,14 +525,6 @@ new Vue({
         });
       }
     },
-    peerEditRemoveConnections(connectionId) {
-      if (this.peerEditStaticConnectionIds.includes(connectionId)) {
-        this.peerEditStaticConnectionIds.splice(this.peerEditStaticConnectionIds.indexOf(connectionId), 1);
-      }
-      if (this.peerEditRoamingConnectionIds.includes(connectionId)) {
-        this.peerEditRoamingConnectionIds.splice(this.peerEditRoamingConnectionIds.indexOf(connectionId), 1);
-      }
-    },
     async peerConfigEditUpdateConfirmation() {
       const [changedFields, errorNotFound] = this.peerEditChangedFieldsCompute;
       if (!errorNotFound || Object.keys(changedFields).length === 0) return;
